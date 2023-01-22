792a4d5270a7355c1b0475004e17cd310dcb5dfc
Jeylal <99549192+Jeylal@users.noreply.github.com>
2023-01-22 16:20:00 +0100

    update

--- app.R
+++ app.R
@@ -8,26 +8,44 @@
 #
 
 library(shiny)
+library(shiny)
+library(shinythemes)
+library(rsconnect)
+library(tidyverse)
+library(sjlabelled)
+library(viridis)
+library(haven)
+library(shinyWidgets)
+library(shinysurveys)
+library(readr)
+
+setwd("F:/social survey website project/data")
+religion2018 <- read_rds("religion2018.rds")
+
+choicecountry <- religion2018 %>% count(c_alphan)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Social Survey Data explorer"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            varSelectInput("religionquestion",
+                        label = p("Question"),
+                        data = religion2018[, c(3:372)],
+                        multiple = FALSE),
+            selectInput("countryreligion",
+                        label = p("Country"),
+                        choices = choicecountry[, 1],
+                        multiple = TRUE)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("barplotreli")
         )
     )
 )
@@ -35,15 +53,26 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+  barplotreli <- reactive({
+    religion2018 %>% 
+      filter(c_alphan %in% input$countryreligion) %>% 
+      ggplot()+
+      aes(y = !!input$religionquestion, x = after_stat(count/sum(count)), fill = c_alphan)+
+      geom_bar(col="black", alpha=0.8, width = 0.8, na.rm = TRUE, position = "dodge")+
+      scale_x_continuous(position = "top",
+                         expand = c(0.001,0))+
+      xlab("")+
+      ylab("")+
+      scale_fill_viridis(option = "turbo", discrete = TRUE)+
+      ggtitle(input$religionquestion)
+      
+  })
+  
+  
+  
+  
+    output$barplotreli <- renderPlot({
+      barplotreli()
     })
 }
 
